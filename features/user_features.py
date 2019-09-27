77d5e505d35bd5b5c403e12b5101bc666b961803
evvfebruary <Firebulletred14Yn>
2019-09-27 22:20:19 +0300

    # Async added

--- features/user_features.py
+++ features/user_features.py
@@ -1,14 +1,13 @@
 import aiovk
 
 
-def photos_info_by_user(data="urls", user_id=27196901):
+async def photos_info_by_user(data="urls", user_id=27196901):
     # ToDo: Remove after normal auth
     app_secret = "x49X1eyoU1BTPrFsBBVx"
     app_id = 7150578
     app_service = "dbfc62d1dbfc62d1dbfc62d140db917923ddbfcdbfc62d186725cfca26ef9526763372e"
     with aiovk.ImplicitSession("89851301115", "evvdatascience", app_id, "groups,friends,photos, albums") as session:
         api = aiovk.API(session)
-
         photo_infos = []
         albums_request = api.photos.getAlbums(owner_id=user_id)
         albums = await albums_request
