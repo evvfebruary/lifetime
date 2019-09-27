81414b5ec6fabe7e01dc2b1fb37069e802c54f0b
evvfebruary <Firebulletred14Yn>
2019-09-27 21:59:39 +0300

    # Checks

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import aiovk
+import features.user_features as uf
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route("/userfeatures/photos")
+def photos_info_by_user():
+    photos_info = uf.photos_info_by_user()
+    return jsonify(photos_info)
 
 
 if __name__ == '__main__':
