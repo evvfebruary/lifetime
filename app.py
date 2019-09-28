d24b18b1995792ffea8994a30c6723530d1bd12b
evvfebruary <Firebulletred14Yn>
2019-09-29 01:09:21 +0300

    # Dash info logic

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import aiovk
 import features.user_features as uf
 import features.weather as wth
+import os
 from quart import Quart, request, jsonify, send_file
 import glob
 
 app = Quart(__name__)
-
+dash_index = 0
 
 @app.route('/wthdash')
 async def hello_world():
+    dash_info = ["weather", "sea_temp"]
     form = request.args
     destination = form.get('destination')
+    files = [f"/home/ubuntu/lifetime/dashboards/weather/{destination}_{info}.png" for info in dash_info if os.path.exists(f"/home/ubuntu/lifetime/dashboards/weather/{destination}_{info}.png")]
     # dashboard = await send_file(f"/Users/evv/PycharmProjects/lifetime/dashboards/weather/{destination}_weather.png")
-    return await send_file(f"/home/ubuntu/lifetime/dashboards/weather/{destination}_weather.png")
+    return await send_file(files[dash_index % len(files)])
     # return await send_from_directory("/Users/evv/PycharmProjects/lifetime/dashboards/weather", f"{destination}_weater")
 
 
