9cd74e7f206be80ba12a38f593c849f13cee7bb7
evvfebruary <Firebulletred14Yn>
2019-09-29 05:00:30 +0300

    # Fixed for service key try

--- features/user_features.py
+++ features/user_features.py
@@ -35,28 +35,15 @@ async def group_txt_photos_by_user(user_id=27196901):
     return text_info_groups
 
 
-async def photos_info_by_user(data="urls", user_id=27196901):
-    # ToDo: Remove after normal auth
-    app_secret = "x49X1eyoU1BTPrFsBBVx"
-    app_id = 7150578
-    app_service = "dbfc62d1dbfc62d1dbfc62d140db917923ddbfcdbfc62d186725cfca26ef9526763372e"
-    session = aiovk.ImplicitSession("89851301115", "evvdatascience", app_id, "groups,friends,photos, albums")
-    api = aiovk.API(session)
-    # ToDo: Remove after normal auth
+async def photos_info_by_user(data="urls", user_id=317799):
     photo_infos = []
+    session = aiovk.TokenSession(access_token='dbfc62d1dbfc62d1dbfc62d140db917923ddbfcdbfc62d186725cfca26ef9526763372e')
+    api = aiovk.API(session)
     albums_request = api.photos.getAlbums(owner_id=user_id)
     albums = await albums_request
-    album_ids = [album_info["id"] for album_info in albums.get("items")]
+    album_ids = [album_info["id"] for album_info in albums.get("items")][:2]
     for album_id_cs in album_ids:
-        photos_by_album = await api.photos.get(album_id=album_id_cs)
+        photos_by_album = await api.photos.get(owner_id=user_id, album_id=album_id_cs)
         photo_infos += [{"url": photo_info["photo_604"], "date": photo_info['date']} for photo_info in
                         photos_by_album.get("items")]
-
-    print(photo_infos)
-    await session.close()
-    if data is None:
-        return photo_infos
-    elif data == "urls":
-        return [photo_info["url"] for photo_info in photo_infos]
-    elif data == "full":
-        return photo_infos
+    return [photo['url'] for photo in photo_infos]
