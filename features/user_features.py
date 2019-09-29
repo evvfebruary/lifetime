b015d1dcbc4dc4741aa9e2853ccdd52db9a4e6da
evvfebruary <Firebulletred14Yn>
2019-09-29 05:55:59 +0300

    # Texts get

--- features/user_features.py
+++ features/user_features.py
@@ -17,21 +17,17 @@ async def get_post_urls(group_id, vk_api):
     return post_urls
 
 
-async def group_txt_photos_by_user(user_id=27196901):
-    # ToDo: Remove after normal auth
-    app_secret = "x49X1eyoU1BTPrFsBBVx"
-    app_id = 7150578
-    app_service = "dbfc62d1dbfc62d1dbfc62d140db917923ddbfcdbfc62d186725cfca26ef9526763372e"
-    session = aiovk.ImplicitSession("89851301115", "evvdatascience", app_id, "groups,friends,photos, albums")
+async def group_txt_photos_by_user(user_id):
+    session = aiovk.TokenSession(access_token='dbfc62d1dbfc62d1dbfc62d140db917923ddbfcdbfc62d186725cfca26ef9526763372e')
     api = aiovk.API(session)
-    # ToDo: Remove after normal auth
-    groups_info = await api.users.getSubscriptions(user_ids=user_id)
+    groups_info = await api.users.getSubscriptions(user_id=user_id)
     groups_ids = groups_info['groups']['items']
     groups_info_parsed = await api.groups.getById(group_ids=",".join([str(each) for each in groups_ids]), fields = "description")
-    text_info_groups = [{"group_id": each["id"], "name":each["name"], "description":each["description"]} for each in groups_info_parsed]
+    text_info_groups = [{"group_id":each["id"], "name":each["name"], "description":each["description"]} for each in groups_info_parsed]
     for group in text_info_groups:
             group['post_photos'] = await get_post_urls(group['group_id'], api)
             time.sleep(1)
+    await session.close()
     return text_info_groups
 
 
@@ -46,4 +42,5 @@ async def photos_info_by_user(data="urls", user_id=317799):
         photos_by_album = await api.photos.get(owner_id=user_id, album_id=album_id_cs)
         photo_infos += [{"url": photo_info["photo_604"], "date": photo_info['date']} for photo_info in
                         photos_by_album.get("items")]
+    await session.close()
     return [photo['url'] for photo in photo_infos]
